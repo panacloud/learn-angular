d3f43d03bf3cb763e9ad7d75ca4e69ad39bef0bc
Zia Khan <ziaukhan@hotmail.com>
2016-02-12 12:55:25 +0500

    remove test Rxjs test code from step 0

--- step00_quick_start/app/main.js
+++ step00_quick_start/app/main.js
@@ -1,6 +1,5 @@
-System.register(['angular2/platform/browser', './app.component', 'rxjs/Observable'], function(exports_1) {
-    var browser_1, app_component_1, Observable_1;
-    var button, clicks;
+System.register(['angular2/platform/browser', './app.component'], function(exports_1) {
+    var browser_1, app_component_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -8,15 +7,9 @@ System.register(['angular2/platform/browser', './app.component', 'rxjs/Observabl
             },
             function (app_component_1_1) {
                 app_component_1 = app_component_1_1;
-            },
-            function (Observable_1_1) {
-                Observable_1 = Observable_1_1;
             }],
         execute: function() {
             browser_1.bootstrap(app_component_1.AppComponent);
-            button = document.getElementById('button');
-            clicks = Observable_1.Observable.fromEvent(button, "click");
-            clicks.subscribe(function (x) { return alert("clicked"); }, function (err) { return alert("error"); }, function () { return alert('Completed'); });
         }
     }
 });
