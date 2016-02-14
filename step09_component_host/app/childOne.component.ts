d23b9f0ce97ede43d38918b1ff62eedc97278936
Zia Khan <ziaukhan@hotmail.com>
2016-02-15 01:53:03 +0500

    added step 9

---
+++ step09_component_host/app/childOne.component.ts
@@ -0,0 +1,21 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'child-one',
+    template: `<div>This is child One with input: {{data}}</div>`,
+    inputs: ['data'],
+    host: {
+      class: 'row',
+      "[style.color]":"backgroundColor()" 
+    }
+})
+export class ChildOneComponent { 
+    data: string;
+    constructor() {
+      this.data = "default value";
+    }
+    
+    backgroundColor(): string {
+        return "red";
+    }
+}
