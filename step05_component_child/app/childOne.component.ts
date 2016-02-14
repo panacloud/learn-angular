2204ba77667159f0fc6e8ee8eef4aae0da0ef13d
Zia Khan <ziaukhan@hotmail.com>
2016-02-14 10:59:39 +0500

    completed step 5

---
+++ step05_component_child/app/childOne.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'child-one',
+    template: `<div>This is child One</div>`
+})
+export class ChildOneComponent { 
+
+    constructor() {
+     
+    }
+}
