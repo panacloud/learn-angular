2cd07eb45d457dcc0a25ecae0e80e2bc17daa9a5
Zia Khan <ziaukhan@hotmail.com>
2016-02-26 15:26:22 +0500

    added step 12

---
+++ step12_ngIf/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<div *ngIf="false">This will not show</div>
+              <div *ngIf="true">This will show</div>
+              <div *ngIf="a > b">This will show because 5 is greater then 4</div>
+              <div *ngIf="myFunction()">This will show because function returns a true value</div>
+    `
+})
+export class AppComponent { 
+    a: number;
+    b: number;
+    
+    constructor(){
+        this.a = 5;
+        this.b = 4;
+    }
+    
+    myFunction(){
+        return true;
+    }
+}
---
+++ step12_ngIf/app/main.ts
@@ -0,0 +1,8 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/> 
+
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+
+bootstrap(AppComponent);
+
