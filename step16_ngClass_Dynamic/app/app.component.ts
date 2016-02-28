84ea54f0204f65d885d8a5b4084b80e5f4d1d2b5
Zia Khan <ziaukhan@hotmail.com>
2016-02-28 12:47:15 +0500

    added step 16

---
+++ step16_ngClass_Dynamic/app/app.component.ts
@@ -0,0 +1,30 @@
+import {Component} from 'angular2/core';
+import {NgClass} from 'angular2/common';
+
+@Component({
+    selector: 'my-app',
+    template: `
+            <div [ngClass]="{bordered: isBordered}">This is text</div>
+            <button (click)=toggleBorder()>Toogle Border</button>
+    `,
+    styles: [`
+        .bordered {
+            border: 1px dashed black;
+            background-color: #eee;
+        }
+  `]
+    directives: [NgClass]
+})
+export class AppComponent { 
+    
+    isBordered: boolean;
+    
+    constructor(){
+        this.isBordered = true;
+    }
+    
+    toggleBorder(){
+        this.isBordered = !this.isBordered;
+    }
+    
+}
---
+++ step16_ngClass_Dynamic/readme.md
@@ -0,0 +1,3 @@
+Chapter 4 page 114 of ng-book 2: 
+
+https://www.ng-book.com/2/
