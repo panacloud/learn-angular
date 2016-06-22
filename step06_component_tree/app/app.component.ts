fd7ef9d42659f99d44c2f84d72dbce98f6360bb2
Zia Khan <ziaukhan@hotmail.com>
2016-06-22 08:47:08 +0500

    updated step 6

--- step06_component_tree/app/app.component.ts
+++ step06_component_tree/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 import {ChildOneComponent} from './childOne.component';
 import {ChildTwoComponent} from './childTwo.component';
 
