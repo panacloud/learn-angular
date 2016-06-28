e996ef6530f2275b24fda961b594550c14a385b1
Zia Khan <ziaukhan@hotmail.com>
2016-06-28 10:38:46 +0500

    added step 21 covering rc 3 forms

---
+++ step21_forms_basic/app/Hero.js
@@ -0,0 +1,10 @@
+"use strict";
+var Hero = (function () {
+    function Hero(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=Hero.js.map
