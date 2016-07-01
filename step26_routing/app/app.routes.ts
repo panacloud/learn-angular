62800301a3c713477fa826749048bbcf394234c7
Zia Khan <ziaukhan@hotmail.com>
2016-07-01 09:05:46 +0500

    updated step 26 to rc 4

--- step26_routing/app/app.routes.ts
+++ step26_routing/app/app.routes.ts
@@ -2,7 +2,7 @@ import { provideRouter, RouterConfig }  from '@angular/router';
 import { Page1Component } from './page1.component';
 import { Page2Component } from './page2.component';
 
-const routes: RouterConfig = [
+export const routes: RouterConfig = [
   {
     path: 'page1',
     component: Page1Component
