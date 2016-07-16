31bfde48316ff33d4e5c385e76699f9f80788000
Zia Khan <ziaukhan@hotmail.com>
2016-07-16 11:53:41 +0500

    added step 56

---
+++ step56_firebase/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>My First Angular 2 App</h1>
+    <ul>
+    <li class="text" *ngFor="let item of items | async">
+      {{item.$value}}
+  </li>
+</ul>
+  `
+})
+export class AppComponent { 
+  af: AngularFire;
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {
+    this.af = af;
+    this.items = af.database.list('items');
+  }
+
+}
+
---
+++ step56_firebase/app/main.ts
@@ -0,0 +1,32 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
+
+/*
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "<your-key>",
+    authDomain: "<your-project-authdomain>",
+    databaseURL: "<your-database-URL>",
+    storageBucket: "<your-storage-bucket>"
+  })
+]);
+*/
+
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "AIzaSyBfPPVdQLUnAq2yJC3yEg2rEdclksJdxvA",
+    authDomain: "quickstart-db.firebaseapp.com",
+    databaseURL: "https://quickstart-db.firebaseio.com",
+    storageBucket: "quickstart-db.appspot.com"
+  })
+]);
+
+
+
+
+
