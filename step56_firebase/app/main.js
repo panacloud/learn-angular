21b111dbf4563810a8d53cb4ea51ad0db1184e16
Zia Khan <ziaukhan@hotmail.com>
2016-07-16 11:58:55 +0500

    updated again

--- step56_firebase/app/main.js
+++ step56_firebase/app/main.js
@@ -2,26 +2,14 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
 var angularfire2_1 = require('angularfire2');
-/*
-bootstrap(AppComponent, [
-  FIREBASE_PROVIDERS,
-  // Initialize Firebase app
-  defaultFirebase({
-    apiKey: "<your-key>",
-    authDomain: "<your-project-authdomain>",
-    databaseURL: "<your-database-URL>",
-    storageBucket: "<your-storage-bucket>"
-  })
-]);
-*/
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     angularfire2_1.FIREBASE_PROVIDERS,
     // Initialize Firebase app  
     angularfire2_1.defaultFirebase({
-        apiKey: "AIzaSyBfPPVdQLUnAq2yJC3yEg2rEdclksJdxvA",
-        authDomain: "quickstart-db.firebaseapp.com",
-        databaseURL: "https://quickstart-db.firebaseio.com",
-        storageBucket: "quickstart-db.appspot.com"
+        apiKey: "<your-key>",
+        authDomain: "<your-project-authdomain>",
+        databaseURL: "<your-database-URL>",
+        storageBucket: "<your-storage-bucket>"
     })
 ]);
 //# sourceMappingURL=main.js.map
