4aa37217775e66117ba2ead5a9068cccb05543b7
Zia Khan <ziaukhan@hotmail.com>
2016-07-16 11:55:34 +0500

    update

--- step56_firebase/app/main.ts
+++ step56_firebase/app/main.ts
@@ -2,7 +2,7 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
-/*
+
 bootstrap(AppComponent, [
   FIREBASE_PROVIDERS,
   // Initialize Firebase app  
@@ -13,18 +13,8 @@ bootstrap(AppComponent, [
     storageBucket: "<your-storage-bucket>"
   })
 ]);
-*/
 
-bootstrap(AppComponent, [
-  FIREBASE_PROVIDERS,
-  // Initialize Firebase app  
-  defaultFirebase({
-    apiKey: "AIzaSyBfPPVdQLUnAq2yJC3yEg2rEdclksJdxvA",
-    authDomain: "quickstart-db.firebaseapp.com",
-    databaseURL: "https://quickstart-db.firebaseio.com",
-    storageBucket: "quickstart-db.appspot.com"
-  })
-]);
+
 
 
 
