9746c3f1c5e573f128b536dcf3721713e96dcdb0
Zia Khan <ziaukhan@hotmail.com>
2016-08-11 17:38:11 +0500

    updated steps 1, 2 and 3 to rc 5

--- step03_events/app/main.js
+++ step03_events/app/main.js
@@ -1,5 +1,5 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_component_1 = require('./app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+var app_module_1 = require('./app.module');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
--- step03_events/app/main.ts
+++ step03_events/app/main.ts
@@ -1,6 +1,3 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-
-bootstrap(AppComponent);
-
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
